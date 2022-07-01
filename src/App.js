f0c8862ac6e6e9bd10052cab6dc0ba7b8c3c928a
Anurag upadhyay <anuragupadhyay319@gmail.com>
2022-07-02 02:36:41 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,45 @@
 import { useState } from 'react';
 import DataProvider from './context/DataProvider';
 
-import {BrowserRouter, Routes, Route, Navigate, Outlet} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate, Outlet } from 'react-router-dom';
 
 //components
 import Login from './components/Accounts/Login';
 import Home from './components/home/Home';
 import Header from './components/header/Header';
 import CreatePost from './components/create/CreatePost';
+import DetailView from './components/details/DetailView';
 
 const PrivateRoute = ({ isAuthenticated, ...props }) => {
   // const token = sessionStorage.getItem('accessToken');
-  return isAuthenticated ? 
+  return isAuthenticated ?
     <>
-      <Header/>
+      <Header />
       <Outlet />
     </> : <Navigate replace to='/login' />
 };
 
 function App() {
 
-  const [isAuthenticated,isUserAuthenticated] = useState(false);
+  const [isAuthenticated, isUserAuthenticated] = useState(false);
   return (
-    
+
     <DataProvider>
       <BrowserRouter>
-        <div style = {{marginTop : 64 }}>
+        <div style={{ marginTop: 64 }}>
           <Routes>
-            <Route path='/login' element={<Login isUserAuthenticated = {isUserAuthenticated} />} />
+            <Route path='/login' element={<Login isUserAuthenticated={isUserAuthenticated} />} />
 
             <Route path='/' element={<PrivateRoute isAuthenticated={isAuthenticated} />} >
               <Route path='/' element={<Home />} />
             </Route>
+
             <Route path='/create' element={<PrivateRoute isAuthenticated={isAuthenticated} />} >
               <Route path='/create' element={<CreatePost />} />
+            </Route>
+
+            <Route path='/details/:id' element={<PrivateRoute isAuthenticated={isAuthenticated} />} >
+              <Route path='/details/:id' element={<DetailView />} />
             </Route>
 
           </Routes>
@@ -44,3 +50,4 @@ function App() {
 }
 
 export default App;
+
