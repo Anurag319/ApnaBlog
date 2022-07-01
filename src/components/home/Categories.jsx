42c121a50874e34026b46265791aae3d0060f057
Anurag upadhyay <anuragupadhyay319@gmail.com>
2022-07-01 13:27:12 +0530

    Different categories of the blogs

--- src/components/home/Categories.jsx
+++ src/components/home/Categories.jsx
@@ -1,6 +1,6 @@
 
 import { Button, Table, TableHead, TableRow, TableCell, TableBody, styled } from '@mui/material';
-// import { Link, useSearchParams } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 
 import { categories } from '../../constants/data';
 
@@ -15,28 +15,28 @@ const StyledButton = styled(Button)`
     color: #fff;
 `;
     
-// const StyledLink = styled(Link)`
-//     text-decoration: none;
-//     color: inherit;
-// `;
+const StyledLink = styled(Link)`
+    text-decoration: none;
+    color: inherit;
+`;
 
 const Categories  = () => {
-    // const [searchParams] = useSearchParams();
-    // const category = searchParams.get('category');
+    const [searchParams] = useSearchParams();
+    const category = searchParams.get('category');
     
     return (
         <>
-            {/* <Link to={`/create?category=${category || ''}`} style={{ textDecoration: 'none' }}> */}
+            <Link to={`/create?category=${category || ''}`} style={{ textDecoration: 'none' }}>
                 <StyledButton variant="contained">Create Blog</StyledButton>
-            {/* </Link> */}
+            </Link>
             
             <StyledTable>
                 <TableHead>
                     <TableRow>
                         <TableCell>
-                            {/* <StyledLink to={"/"}> */}
+                            <StyledLink to={"/"}>
                                 All Categories
-                            {/* </StyledLink> */}
+                            </StyledLink>
                         </TableCell>
                     </TableRow>
                 </TableHead>
@@ -45,9 +45,9 @@ const Categories  = () => {
                         categories.map(category => (
                             <TableRow key={category.id}>
                                 <TableCell>
-                                    {/* <StyledLink to={`/?category=${category.type}`}> */}
+                                    <StyledLink to={`/?category=${category.type}`}>
                                         {category.type}
-                                    {/* </StyledLink> */}
+                                    </StyledLink>
                                 </TableCell>
                             </TableRow>
                         ))
